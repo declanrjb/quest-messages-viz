6c34495270f0a765a35936ad7ec7a9119e433342
declanrjb <declanrjb@gmail.com>
2023-12-18 23:12:00 -0500

    updated json

--- script.js
+++ script.js
@@ -13,15 +13,80 @@ function intToPixels(int) {
     return int;
 }
 
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+/* document.getElementsByTagName('body')[0].style.overflow = 'hidden'; */
+
+var data = JSON.parse(Get("https://raw.githubusercontent.com/declanrjb/quest-messages-viz/main/quest_harassment.json"));
+
+var data_indices = []
+
+for (var key in data) {
+    data_indices.push(key);
+}
+
+shuffle(data_indices);
+
+for (var z=0; z < data_indices.length; z++) {
+    var x = data_indices[z];
+    var message_text = data[x]["Text"];
+    var new_message_obj = document.createElement("p");
+    new_message_obj.className = "message";
+    new_message_obj.innerText = message_text;
+    var doc_body = document.querySelector("body");
+    doc_body.appendChild(new_message_obj);
+    new_message_obj.style.zIndex = z;
+    if (data[x]["Type"] == "Named") {
+        new_message_obj.style.backgroundColor = "black";
+        new_message_obj.style.color = "white";
+    }
+}
+
 var messages = document.querySelectorAll(".message");
 for (var j=0; j < messages.length; j++) {
-    messages[j].style.top = intToPixels(getRandomInt(100));
-    messages[j].style.width = intToPixels(randomIntInRange(200,500));
+    var item_height = messages[j].clientHeight;
+    item_height = parseInt(item_height);
+    messages[j].style.top = intToPixels(getRandomInt(3000) - (item_height / 2));
+    messages[j].style.width = intToPixels(randomIntInRange(500,1000));
     var item_width = messages[j].style.width;
     item_width = parseInt(item_width);
     var max_position = screen.width - item_width;
-    messages[j].style.left = intToPixels(randomIntInRange(0,max_position));
+    var mid_screen = screen.width / 2;
+    messages[j].style.left = intToPixels((mid_screen + randomIntInRange(-300,300)) - (item_width / 2));
     var zdex = randomIntInRange(0,100);
-    messages[j].style.opacity = zdex / 100;
-    messages[j].style.zIndex = zdex;
 }
+
+var scroll_prev = window.scrollY;
+document.addEventListener('scroll', function(e){
+    var scroll_amount = window.scrollY - scroll_prev;
+    scroll_prev = window.scrollY;
+    for (var i=0; i < messages.length; i++) {
+        var message_obj = messages[i];
+        var zdex = parseInt(message_obj.style.zIndex);
+        var curr_top = parseInt(message_obj.style.top);
+        message_obj.style.top = intToPixels(curr_top - ((scroll_amount * (zdex / messages.length))));
+    }
+}, true);
