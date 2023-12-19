a8d78da8994938dea045aa8253c84522a91c1912
declanrjb <declanrjb@gmail.com>
2023-12-19 13:32:04 -0500

    script updates

--- script.js
+++ script.js
@@ -36,7 +36,28 @@ function shuffle(array) {
     }
   
     return array;
-  }
+}
+
+function focus(elem) {
+    var y_center = (window.innerHeight / 2) + window.scrollY;
+    var x_center = window.innerWidth / 2
+    elem.style.left = intToPixels(x_center - (elem.offsetWidth / 2));
+    elem.style.top = intToPixels(y_center - (elem.offsetHeight / 2) - 20);
+    elem.style.zIndex = 1000;
+    for (var q=0; q < messages.length; q++) {
+        if (messages[q] != elem) {
+            messages[q].style.opacity = "0.25";
+        }
+    }
+}
+
+function randItem(list) {
+    return list[Math.floor(Math.random()*list.length)];
+}
+
+function testFunc() {
+    console.log(this.innerText);
+}
 
 /* document.getElementsByTagName('body')[0].style.overflow = 'hidden'; */
 
@@ -50,6 +71,7 @@ for (var key in data) {
 
 shuffle(data_indices);
 
+var z_counter = 0;
 for (var z=0; z < data_indices.length; z++) {
     var x = data_indices[z];
     var message_text = data[x]["Text"];
@@ -58,18 +80,20 @@ for (var z=0; z < data_indices.length; z++) {
     new_message_obj.innerText = message_text;
     var doc_body = document.querySelector("body");
     doc_body.appendChild(new_message_obj);
-    new_message_obj.style.zIndex = z;
+    new_message_obj.style.zIndex = z_counter;
+    z_counter += randomIntInRange(1,5);
     if (data[x]["Type"] == "Named") {
         new_message_obj.style.backgroundColor = "black";
         new_message_obj.style.color = "white";
     }
+    new_message_obj.onclick = testFunc;
 }
 
 var messages = document.querySelectorAll(".message");
 for (var j=0; j < messages.length; j++) {
     var item_height = messages[j].clientHeight;
     item_height = parseInt(item_height);
-    messages[j].style.top = intToPixels(getRandomInt(3000) - (item_height / 2));
+    messages[j].style.top = intToPixels(randomIntInRange(500,5000) - (item_height / 2));
     messages[j].style.width = intToPixels(randomIntInRange(500,1000));
     var item_width = messages[j].style.width;
     item_width = parseInt(item_width);
@@ -79,6 +103,14 @@ for (var j=0; j < messages.length; j++) {
     var zdex = randomIntInRange(0,100);
 }
 
+/*
+addEventListener("click", (event) => {
+    focus(randItem(messages));
+});
+*/
+
+
+
 var scroll_prev = window.scrollY;
 document.addEventListener('scroll', function(e){
     var scroll_amount = window.scrollY - scroll_prev;
@@ -87,6 +119,6 @@ document.addEventListener('scroll', function(e){
         var message_obj = messages[i];
         var zdex = parseInt(message_obj.style.zIndex);
         var curr_top = parseInt(message_obj.style.top);
-        message_obj.style.top = intToPixels(curr_top - ((scroll_amount * (zdex / messages.length))));
+        message_obj.style.top = intToPixels(curr_top - ((scroll_amount * (zdex / z_counter))));
     }
 }, true);
